6d15f25074d80b834da8d4913876b6de4828ccd7
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2024-05-08 21:20:55 +0200

    Basic training scaffolding

--- nerf2d.py
+++ nerf2d.py
@@ -1,6 +1,9 @@
+import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
-from einops import repeat
+import torchmetrics
+from einops import repeat, rearrange
+from torch import nn, Tensor
 
 def pixel_centers(lo, hi, n_pixels):
     """
@@ -13,7 +16,7 @@ def pixel_centers(lo, hi, n_pixels):
     pixel_w = (hi - lo) / n_pixels
     return torch.linspace(lo, hi - pixel_w, n_pixels) + pixel_w / 2
 
-def get_rays2d(height, focal_length_px, c2w):
+def get_rays2d(height: int, focal_length_px: float, c2w: torch.Tensor):
     """
     Compute the set of rays for a 2D camera
     :param height: amount of pixels
@@ -41,3 +44,68 @@ def get_rays2d(height, focal_length_px, c2w):
     directions = directions @ rot_c2w.T
 
     return origins, directions
+
+class NeRF2D_LightningModule(pl.LightningModule):
+
+    def __init__(
+            self,
+            lr=1e-4,
+            t_near=2,
+            t_far=6
+    ):
+        super().__init__()
+
+        self.save_hyperparameters()
+
+        # dummy model
+        self.model = nn.Linear(3, 4)
+
+        # metrics
+        self.train_mse = torchmetrics.MeanSquaredError()
+
+    def render_view(self, height: int, focal_length_px: float, c2w: Tensor):
+        """
+        Render a view from a 2D camera
+        :param height:
+        :param focal_length_px:
+        :param c2w:
+        :return:
+        """
+
+        # render rays for view
+        colors = self.forward(*get_rays2d(height, focal_length_px, c2w))
+
+        # reshape as image
+        colors_im = rearrange(colors, 'h c -> h 1 c').detach().numpy()
+        return colors_im
+
+    def forward(
+            self,
+            origins: Tensor,
+            directions: Tensor,
+    ) -> Tensor:
+        """
+        Perform volume rendering on model given a set of rays
+        :param origins: origins of rays N, 3
+        :param directions: directions of rays N, 3
+        :return: rendered-colors at rays N, 3
+        """
+
+        # TODO replace with volume rendering
+        return F.sigmoid(torch.randn(origins.shape[0], 3, requires_grad=True).to(origins))
+
+    def training_step(self, batch, batch_idx):
+        origins, directions, colors = batch
+
+        # forward pass
+        colors_pred = self.nerf_forward(origins, directions)
+
+        # compute loss
+        loss = self.train_mse(colors_pred, colors)
+
+        self.log('train_loss', loss)
+
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
